30389cc99b2ec790478ca68c5e0eb1cd48355705
dmyrych <dmyrych17@gmail.com>
2022-08-27 22:08:42 +0300

    second commit

--- app/src/main/java/gitTraining/App.java
+++ app/src/main/java/gitTraining/App.java
@@ -1,11 +1,8 @@
 package gitTraining;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
+    
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.print("Hello world!");
     }
 }
